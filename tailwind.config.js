a1d2eebb3cbb80724483c0598ea9b6173e2ac3b2
MeesamBukhari <anneciota@users.noreply.github.com>
2026-08-19 23:16:23 +0500

    feat: enhance hero section with full sunset background and updated typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
       fontFamily: {
         heading: ['"Playfair Display"', 'serif'],
         body: ['Inter', 'sans-serif'],
+        script: ['"Dancing Script"', 'cursive'],
       },
     },
   },
